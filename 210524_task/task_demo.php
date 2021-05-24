a054ed3071cc673e1a04f3474c8af65c047275b0
libin <bin.li@gaea.com>
2021-05-24 14:50:52 +0800

    swoole的task测试

---
+++ 210524_task/task_demo.php
@@ -0,0 +1,30 @@
+<?php
+
+$serv = new Swoole\Server('127.0.0.1', 9601);
+
+//设置异步任务的工作进程数量
+$serv->set([
+    'task_worker_num' => 4
+]);
+
+//此回调函数在worker进程中执行
+$serv->on('Receive', function ($serv, $fd, $reactor_id, $data) {
+    //投递异步任务
+    $task_id = $serv->task($data);
+    echo "代码继续执行中: id={$task_id}\n";
+});
+
+//处理异步任务(此回调函数在task进程中执行)
+$serv->on('Task', function ($serv, $task_id, $reactor_id, $data) {
+    echo "正在处理异步任务[id={$task_id}]" . PHP_EOL;
+    sleep(6);// 睡眠中，模拟任务处理
+    //返回任务执行的结果
+    $serv->finish("{$data} -> OK");
+});
+
+//处理异步任务的结果(此回调函数在worker进程中执行)
+$serv->on('Finish', function ($serv, $task_id, $data) {
+    echo "异步任务执行完成咯[{$task_id}] Finish: {$data}" . PHP_EOL;
+});
+
+$serv->start();
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # swoole 测试代码
+
+## 210427_listen
+
+swoole 端口监听，通过监听的端口 shutdown 掉 swoole 的服务
+
+## 210519_event
+
+测试 php 原生的 event 事件
+
+## 210520_pcntl
+
+测试 php 原生的进程管理，和进程 fork
+
+## 210524_task
+
+swoole 的 task 异步任务测试
