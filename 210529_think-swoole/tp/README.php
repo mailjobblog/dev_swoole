572f1fa92c7d47e3d691acf175a20e993f5b8768
libin <bin.li@gaea.com>
2021-05-29 14:11:16 +0800

    push

---
+++ 210529_think-swoole/tp/README.php
@@ -0,0 +1,28 @@
+## ThinkPHP 6.0 测试 think-swoole
+
+## 框架安装
+
+### 安装以来
+
+```
+composer update
+```
+
+#### 测试框架是否安装成功
+
+进入命令行下面，执行下面指令
+
+```
+php think run
+```
+
+在浏览器中输入地址：
+
+```
+http://localhost:8000/
+```
+
+#### 测试swoole是否安装成功
+
+https://www.kancloud.cn/manual/thinkphp6_0/1359700
+
